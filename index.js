6fe436a3277df7a1210fc41cf916ff0dbaf1a217
wongcmichelle <Michelle@Michelles-MacBook-Pro-2.local>
2019-05-01 06:45:33 -0400

    Fix webhook call to api

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ server.use(bodyParser.urlencoded({
     extended: true
 }));
 
-server.use(bodyParser.json());
+server.use(bodyParser.json({ type: "*/*" }));
 
-server.post('/get-food-recipes', function (request,response)  {
-    if (request.body.result && request.body.result.parameters && request.body.result.parameters['ingredients']) {
+server.post('/get-food-recipes', function (request, response)  {
+    if (request.body && request.body.result && request.body.result.parameters && request.body.result.parameters['ingredients']) {
             const ingredients = request.body.result.parameters['ingredients'];
-            const reqUrl = encodeURI(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingredients}`)
-            var req = unirest("GET", reqUrl);
+            const reqUrl = encodeURI(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=${ingredients}`)            
+            var req = unirest.get(reqUrl);
             req.headers({
                 "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
                 "X-RapidAPI-Key": API_KEY
@@ -22,24 +22,27 @@ server.post('/get-food-recipes', function (request,response)  {
             req.send("{}");
             req.end(function (res) {
                 if (res.error) {
-                    response.setHeader('Content-Type', 'application/json');
+                    response.header('Content-Type', 'application/json');
                     response.send(JSON.stringify({
                         "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
+                        "displayText" : "Error. Can you try it again ? ",
+                        "source": 'get-food-recipes'
                     }));
-                } else if (res.body.results.length > 0) {
-                    let result = res.body.results;
+                } else if (res.body.length > 0) {
+                    let result = res.body;
                     let output = '';
-                    for(let i = 0; i<result.length;i++) {
-                        const hyphenTitle = result[i].title.toLowerCase().split(' ').join('-');
-                        const id = result[i].id;
+                    for(let i = 0; i < result.length; i++) {
+                        const recipe = result[i]
+                        const hyphenTitle = recipe.title.toLowerCase().split(' ').join('-');
+                        const id = recipe.id;
                         output += `https://spoonacular.com/recipes/${hyphenTitle}-${id}`;
                         output+="\n"
                     }
-                    response.setHeader('Content-Type', 'application/json');
+                    response.header('Content-Type', 'application/json');
                     response.send(JSON.stringify({
                         "speech" : output,
-                        "displayText" : output
+                        "displayText" : output,
+                        "source": 'get-food-recipes'
                     })); 
                 }
             });
